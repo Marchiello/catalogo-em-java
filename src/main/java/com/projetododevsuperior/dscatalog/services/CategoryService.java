2ab9e7f30e37589b5da761f2671dfff1c1216362
Marcello Vinhais <marcellovsfilho@outlook.com>
2025-12-14 21:17:22 -0300

    Isolando chamadas de BD na camada de serviço

--- src/main/java/com/projetododevsuperior/dscatalog/services/CategoryService.java
+++ src/main/java/com/projetododevsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.projetododevsuperior.dscatalog.entities.Category;
 import com.projetododevsuperior.dscatalog.repositories.CategoryRepository;
@@ -14,6 +15,9 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository _repository;
 	
+	@Transactional(readOnly = true) /* Transação com BD */
+	// Isso, alinhado com o open-in-view=false, fazem com que as consultas do BD
+	// estejam isoladas na camada de serviço. O Controlador apenas entrega a resposta.
 	public List<Category> findAll(){
 		return _repository.findAll();
 	}
