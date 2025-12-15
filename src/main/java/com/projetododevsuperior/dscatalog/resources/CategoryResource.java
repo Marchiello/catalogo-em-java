4a987a8753fdb3685e119c03c44edd7585f6caf8
Marcello Vinhais <marcellovsfilho@outlook.com>
2025-12-14 21:33:21 -0300

    DTO

--- src/main/java/com/projetododevsuperior/dscatalog/resources/CategoryResource.java
+++ src/main/java/com/projetododevsuperior/dscatalog/resources/CategoryResource.java
@@ -1,6 +1,5 @@
 package com.projetododevsuperior.dscatalog.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.projetododevsuperior.dscatalog.dto.CategoryDTO;
 import com.projetododevsuperior.dscatalog.entities.Category;
 import com.projetododevsuperior.dscatalog.services.CategoryService;
 
@@ -22,8 +22,8 @@ public class CategoryResource {
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll(){
+		List<CategoryDTO> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
 	}
--- src/main/java/com/projetododevsuperior/dscatalog/services/CategoryService.java
+++ src/main/java/com/projetododevsuperior/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.projetododevsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.projetododevsuperior.dscatalog.dto.CategoryDTO;
 import com.projetododevsuperior.dscatalog.entities.Category;
 import com.projetododevsuperior.dscatalog.repositories.CategoryRepository;
 
@@ -18,8 +20,10 @@ public class CategoryService {
 	@Transactional(readOnly = true) /* Transação com BD */
 	// Isso, alinhado com o open-in-view=false, fazem com que as consultas do BD
 	// estejam isoladas na camada de serviço. O Controlador apenas entrega a resposta.
-	public List<Category> findAll(){
-		return _repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = _repository.findAll();
+		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 	
 }
